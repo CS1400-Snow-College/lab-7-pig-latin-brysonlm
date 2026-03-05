5ad840122a08278cfde2a8282c21610188f9bfeb
Bryson <bryson.leon-menjivar@students.snow.edu>
2026-03-04 23:13:51 -0700

    step 3

--- Program.cs
+++ Program.cs
@@ -9,5 +9,24 @@ string vowels = "aeiouAEIOU";
 
 for (int i = 0; i < words.Length; i++)
 {
+    //grabbing any of the punctuation
     string word = words[i].ToLower().TrimEnd('.', '!', '?');
+    string punctuation = words[i].Substring(word.Length);
+
+    if (vowels.Contains(word[0]))
+    {
+        pigLatinWords[i] = word + "way" + punctuation;
+    }
+    else
+    {
+        int firstVowelIndex = -1;
+        for (int j = 0; j < word.Length; j++)
+        {
+            if (vowels.Contains(word[j]))
+            {
+                firstVowelIndex = j;
+                break;
+            }
+        }
+    }
 }
--- obj/Debug/net10.0/lab-7-pig-latin-brysonlm.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab-7-pig-latin-brysonlm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-7-pig-latin-brysonlm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49755d9c0e9413af240684bbf6df07fe9ec1061d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+748965b66bc77b8a59beef531aaeeedb802f6968")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-7-pig-latin-brysonlm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-7-pig-latin-brysonlm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
