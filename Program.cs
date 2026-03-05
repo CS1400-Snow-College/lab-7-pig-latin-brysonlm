748965b66bc77b8a59beef531aaeeedb802f6968
Bryson <bryson.leon-menjivar@students.snow.edu>
2026-03-04 23:02:11 -0700

    step 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// collect the message
+Console.WriteLine("Enter your message!: ");
+string input = Console.ReadLine();
+
+string[] words = input.Split(' ');
+string[] pigLatinWords = new string[words.Length];
+
+string vowels = "aeiouAEIOU";
+
+for (int i = 0; i < words.Length; i++)
+{
+    string word = words[i].ToLower().TrimEnd('.', '!', '?');
+}
--- obj/Debug/net10.0/lab-7-pig-latin-brysonlm.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab-7-pig-latin-brysonlm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-7-pig-latin-brysonlm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b3cf5ce35aa602a3106cb71b7f42e04a23c87b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49755d9c0e9413af240684bbf6df07fe9ec1061d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-7-pig-latin-brysonlm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-7-pig-latin-brysonlm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
